af5f89cd6b0b7e2a97487b6a1cf2ac78d2b71327
Jonathan Raphaelson <jon@accidental.cc>
2025-05-28 16:11:41 -0600

    server can serve the app after dist

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,5 +13,13 @@ app.use("/api", await router({ directory: routes }));
 const peerServer = ExpressPeerServer(server, {})
 app.use("/sync", peerServer)
 
+// this is the app (placed there by dist script)
+const distAssets = path.join(import.meta.dirname, 'dist')
+app.use(express.static(distAssets))
+
+// this is normal assets
+const publicAssets = path.join(import.meta.dirname, 'public')
+app.use(express.static(publicAssets))
+
 const addr = server.address() as AddressInfo;
 console.log(`server is listening @ http://[${addr.address}]:${addr.port}`)
